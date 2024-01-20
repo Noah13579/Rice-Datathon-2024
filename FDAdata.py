b83f8b91ed1709f027227ab2e21405eebcbade61
BrianoAden <brianoeaden@gmail.com>
2024-01-19 21:16:27 -0600

    Created a few subsets of the provided data

--- FDAdata.py
+++ FDAdata.py
@@ -3,4 +3,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 FDAdata = pd.read_csv('./CAERS_ProductBased.csv')
-print(FDAdata.to_string())
+print(FDAdata.shape)
+FDAdata_SEX_rem = FDAdata.dropna(subset = ["SEX"])
+print(FDAdata_SEX_rem)
+Wo_Products = FDAdata_SEX_rem[FDAdata_SEX_rem["SEX"] == "Female"]
+print(Wo_Products)
