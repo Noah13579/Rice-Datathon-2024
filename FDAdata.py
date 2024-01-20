93ac764eb590d21059ca1b544f4df681e40f531d
angel-romero-f <angelfloromero@gmail.com>
2024-01-19 20:54:27 -0600

    Read file

--- FDAdata.py
+++ FDAdata.py
@@ -2,7 +2,5 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-<<<<<<< HEAD
-FDAdata = pd.read_csv('Rice-Datathon-2024/CAERS_ProductBased.csv')
-=======
->>>>>>> 719d476add321a58c89ddb6595907a4174ac3f18
+FDAdata = pd.read_csv('./CAERS_ProductBased.csv')
+print(FDAdata.to_string())
