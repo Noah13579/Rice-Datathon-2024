f7ec2bdd923c68a133390c782f95e16ff55b1828
angel-romero-f <angelfloromero@gmail.com>
2024-01-19 22:37:26 -0600

    Add filtering

--- FDAdata.py
+++ FDAdata.py
@@ -8,3 +8,11 @@ FDAdata_SEX_rem = FDAdata.dropna(subset = ["SEX"])
 print(FDAdata_SEX_rem)
 women_data = FDAdata_SEX_rem[FDAdata_SEX_rem["SEX"] == "Female"]
 print(women_data)
+occurrence_data = FDAdata["PRODUCT"].value_counts()
+print(occurrence_data)
+#Removes rows that contain EXEMPTION 4 in PRODUCTS column
+cleaned_data = FDAdata[FDAdata["PRODUCT"] != "EXEMPTION 4"]
+print(cleaned_data.shape)
+keep_data = occurrence_data[occurrence_data > 2].index
+finished_data = cleaned_data[cleaned_data["PRODUCT"].isin(keep_data)]
+print(finished_data.shape)
