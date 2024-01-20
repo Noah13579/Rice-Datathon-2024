626c2b256ec758d1fc1061fb071e093bc1ead6a4
angel-romero-f <angelfloromero@gmail.com>
2024-01-20 11:23:00 -0600

    Changed relevant categories to be in lower case

--- FDAdata.py
+++ FDAdata.py
@@ -6,6 +6,11 @@ FDAdata = pd.read_csv('./CAERS_ProductBased.csv')
 FDAdata_SEX_rem = FDAdata.dropna(subset = ["SEX"])
 cleaned_data = FDAdata_SEX_rem[FDAdata_SEX_rem["PRODUCT"] != "EXEMPTION 4"]
 cleaned_data["PRODUCT"] = cleaned_data["PRODUCT"].str.lower()
+cleaned_data["DESCRIPTION"] = cleaned_data["DESCRIPTION"].str.lower()
+cleaned_data["CASE_MEDDRA_PREFERRED_TERMS"] = cleaned_data["CASE_MEDDRA_PREFERRED_TERMS"].str.lower()
+cleaned_data["CASE_OUTCOME"] = cleaned_data["CASE_OUTCOME"].str.lower()
+
+
 occurrence_data = cleaned_data["PRODUCT"].value_counts()
 keep_data = occurrence_data[occurrence_data > 400].index
 finished_data = cleaned_data[cleaned_data["PRODUCT"].isin(keep_data)]
